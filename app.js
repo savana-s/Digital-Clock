133d35f0c2a4cd8fafb1f8d27e1b6ce59b87bcff
savana-s <55608547+savana-s@users.noreply.github.com>
2019-11-12 21:28:56 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function clock() {
+  const fullDate = new Date(); 
+  var hours = fullDate.getHours();
+  var mins = fullDate.getMinutes();
+  var secs = fullDate.getSeconds();
+
+  document.getElementById('hour').innerHTML = hours;
+  document.getElementById('minute').innerHTML = mins;
+  document.getElementById('second').innerHTML = secs;
+}
+
+setInterval(clock,100);
